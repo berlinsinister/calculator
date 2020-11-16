bac66233a347d65d2f4abf603911ae399b6edbfc
berlinsinister <malikaefremova@gmail.com>
2020-11-16 16:06:37 +0400

    Updates

---
+++ precache-manifest.1bce4fa301138f348d753cb6aea074fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa54e3f0594c77df03c0b25e682bae2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "f97dd11da6ec9dd606c5",
+    "url": "/calculator/static/css/main.2ca1e9c1.chunk.css"
+  },
+  {
+    "revision": "72cc69ca1024fbd5fdd0",
+    "url": "/calculator/static/js/2.083d50fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.083d50fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97dd11da6ec9dd606c5",
+    "url": "/calculator/static/js/main.70fc28ce.chunk.js"
+  },
+  {
+    "revision": "d61f49d990cdc483c0da",
+    "url": "/calculator/static/js/runtime-main.2e1c5b08.js"
+  }
+]);
